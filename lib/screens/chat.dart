a3ca8b08a866178ac41ceb7dad7123e66c0654da
unknown <bhavikachuttar@gmail.com>
2020-09-04 22:16:39 +0530

    chat

---
+++ lib/screens/chat.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ChatApp extends StatefulWidget {
+  @override
+  _ChatAppState createState() => _ChatAppState();
+}
+
+class _ChatAppState extends State<ChatApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
